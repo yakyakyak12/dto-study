dfe0218941d570b02852af2a21233ab4c779949d
unjoo31 <unjoo31@gmail.com>
2023-09-19 16:01:08 +0900

    최초업로드

---
+++ src/main/java/com/example/kakao/cart/CartResponse.java
@@ -0,0 +1,16 @@
+package com.example.kakao.cart;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+public class CartResponse {
+
+    // (기능3) 장바구니 조회
+    @ToString
+    @Getter
+    @Setter
+    public static class FindAllByUserDTO {
+       
+    }
+}
